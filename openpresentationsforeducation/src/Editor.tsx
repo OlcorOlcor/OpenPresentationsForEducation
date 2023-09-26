39a7600e745b790f8a95096df3deff7b3881dde0
Jan Čelovský <celovskyhonza@gmail.com>
2023-09-26 15:04:04 +0200

    Add Editor component

---
+++ openpresentationsforeducation/src/Editor.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Editor() {
+    return (
+        <div>
+            Here you will be able to write your own presentations TEST
+        </div>
+    );
+}
+
+export default Editor;
