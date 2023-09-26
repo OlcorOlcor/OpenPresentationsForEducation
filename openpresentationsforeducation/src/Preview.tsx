44fe2226ec20d20328e5b794dd43914b8c0f3d32
Jan Čelovský <celovskyhonza@gmail.com>
2023-09-26 15:05:03 +0200

    Add Preview component

---
+++ openpresentationsforeducation/src/Preview.tsx
@@ -0,0 +1,11 @@
+import react from 'react'
+ 
+function Preview() {
+    return (
+        <div>
+            Here your presentation will be displayed.
+        </div>
+    )
+}
+
+export default Preview;
