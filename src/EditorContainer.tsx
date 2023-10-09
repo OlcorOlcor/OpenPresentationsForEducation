038924ef7d3fbc6ed6397ae3ef04118219ed0de6
Jan Čelovský <celovskyhonza@gmail.com>
2023-10-09 19:00:00 +0200

    Add parsing using marked

--- src/EditorContainer.tsx
+++ src/EditorContainer.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Editor from '@monaco-editor/react';
+import monaco from 'monaco-editor';
+import { marked } from 'marked';
+
+
 
 function EditorContainer() {
+
+    const editorRef = useRef(null);
+
+    function HandleChange() {
+        if (editorRef.current != null) {
+            const editor = editorRef.current as monaco.editor.IStandaloneCodeEditor;
+            const html = marked.parse(editor.getValue());
+            console.log(html);
+        }
+    }
+
+    function HandleMount(editor: any) {
+        editorRef.current = editor;
+    }
+
     return (
-        <Editor height="100%" defaultLanguage="Markdown" />
+        <Editor height="100%" defaultLanguage="Markdown" onMount={HandleMount} onChange={HandleChange}/>
     );
 }
 
