a2c1a5ad812a4f7894a3ebf37db9a22faf80542e
Jan Čelovský <celovskyhonza@gmail.com>
2023-10-29 12:59:05 +0000

    Format code using Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -15,23 +15,28 @@ function App() {
   };
 
   return (
-  <div className="container">
-    <Grid container spacing={2} className="gridContainer">
-      <Grid item xs={6}>
-        <Grid container direction="column" spacing={2} style={{height: '100%'}}>
-          <Grid item xs={8}>
-            <EditorContainer onDataChange={handleDataChange} />
-          </Grid>
-          <Grid item xs={4}>
-            <div>Here the metadata will reside</div>
+    <div className="container">
+      <Grid container spacing={2} className="gridContainer">
+        <Grid item xs={6}>
+          <Grid
+            container
+            direction="column"
+            spacing={2}
+            style={{ height: "100%" }}
+          >
+            <Grid item xs={8}>
+              <EditorContainer onDataChange={handleDataChange} />
+            </Grid>
+            <Grid item xs={4}>
+              <div>Here the metadata will reside</div>
+            </Grid>
           </Grid>
         </Grid>
+        <Grid item xs={6}>
+          <div className="half">{generatedData}</div>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <div className="half">{generatedData}</div>
-      </Grid>
-    </Grid>
-  </div>
+    </div>
   );
 }
 
