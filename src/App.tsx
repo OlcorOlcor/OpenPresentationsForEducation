658b8161da18453def79db41b0b5f7eebbbbefb7
Jan Čelovský <celovskyhonza@gmail.com>
2023-11-30 17:53:40 +0100

    Compile on a button press

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,41 @@
 import React, { useState, useRef } from "react";
 import Grid from "@mui/material/Grid";
 import "./App.css";
-import EditorContainer from "./EditorContainer";
+import EditorContainer, { EditorMethods } from "./EditorContainer";
 import Preview from "./Preview";
-import MetadataContainer, {
-  MetadataContainerMethods,
-} from "./MetadataContainer";
+import { tokenizeText, OpenTagToken, CloseTagToken } from "./AreaTokenizer";
+import MetadataContainer, { MetadataContainerMethods } from "./MetadataContainer";
 import { CustomArea, CustomAreaProcessor } from "./CustomAreaProcessor";
 
 function App() {
-  const areaProcessor: CustomAreaProcessor = new CustomAreaProcessor();
+  const editorContainerRef = useRef<EditorMethods>(null);
   const metadataComponentRef = useRef<MetadataContainerMethods>(null);
   const [generatedData, setGeneratedData] = useState(
     "Here your presentation will be displayed",
   );
-
-  const handleDataChange = (newData: string) => {
-    if (
-      metadataComponentRef.current !== undefined &&
-      metadataComponentRef.current !== null
-    ) {
-      metadataComponentRef.current.updateAreas(areaProcessor.getAreas());
+  function compile() {
+    if (editorContainerRef.current === null) {
+      return;
     }
-    setGeneratedData(newData);
-  };
+    let text = editorContainerRef.current.getData();
+    let tokenArray = tokenizeText(text);
+    
+    //TODO: save to file
+    console.log(tokenArray);
+  }
 
   return (
     <div className="container">
       <Grid container spacing={2} className="gridContainer">
         <Grid item xs={6}>
-          <Grid
-            container
-            direction="column"
-            spacing={2}
-            style={{ height: "100%" }}
-          >
-            <Grid item xs={8}>
-              <EditorContainer
-                onDataChange={handleDataChange}
-                areaProcessor={areaProcessor}
-              />
+          <Grid container direction="column" spacing={2} style={{ height: "100%" }}>
+            <Grid item xs={1}>
+              <button onClick={compile}>Compile</button>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={8}>
+              <EditorContainer ref={editorContainerRef} />
+            </Grid>
+            <Grid item xs={3}>
               <MetadataContainer ref={metadataComponentRef} />
             </Grid>
           </Grid>
