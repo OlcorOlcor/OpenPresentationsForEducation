05eaa624cc8384d16ccee490fde91de926e83347
Jan Čelovský <celovskyhonza@gmail.com>
2023-12-06 16:37:43 +0100

    Remove outdated Tokenizer

--- src/AreaTokenizer.ts
+++ src/AreaTokenizer.ts
@@ -1,35 +1,4 @@
 export type Token = (string | OpenTagToken | CloseTagToken);
-
-export interface AreaToken {
-  type: "area";
-  id: string;
-  name: string;
-  content: string;
-}
-
-export function areaTokenizer(src: string): AreaToken[] {
-  const tokens: AreaToken[] = [];
-  // Matches <foo: 1> lorem </foo>
-  //TODO: Doesn't match overlapping areas
-  const customAreaRegex = /<([\s\S]+): (.+)>([\s\S]*)<\/\1>/g;
-  let match;
-  // Iterates the matches and creates tokens
-  while ((match = customAreaRegex.exec(src)) !== null) {
-    const content = match[3];
-    const id = match[2];
-    const name = match[1];
-
-    const customToken: AreaToken = {
-      type: "area",
-      name: name,
-      id: id,
-      content: content,
-    };
-    tokens.push(customToken);
-  }
-
-  return tokens;
-}
 interface TokenParsingContainer {
   token: OpenTagToken | CloseTagToken;
   length: number;
