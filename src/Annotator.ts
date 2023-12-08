4de6534a84de569c0b107f4083cea87eca4ecf0f
Jan Čelovský <celovskyhonza@gmail.com>
2023-12-08 16:57:06 +0100

    Add annotation of headings

--- src/Annotator.ts
+++ src/Annotator.ts
@@ -1,3 +1,4 @@
+import { isConstructorDeclaration } from "typescript";
 import { OpenTagToken, CloseTagToken, Token } from "./AreaTokenizer";
 
 /**
@@ -22,27 +23,101 @@ function isCloseTagToken(token: Token): token is CloseTagToken {
     return (token as OpenTagToken).id === undefined && (token as CloseTagToken).name !== undefined;
 }
 
-function handleOpenTagToken(annotatedText: string, token: OpenTagToken, tagStack: (OpenTagToken | CloseTagToken)[]): string {
+function handleOpenTagToken(token: OpenTagToken, tagStack: (OpenTagToken | CloseTagToken)[]): string {
     tagStack.push(token);
-
+    let annotatedTag: string = ""
     // TODO: make more unique
-    annotatedText += "<";
-    annotatedText += token.name;
-    annotatedText += ">";
+    annotatedTag += "<";
+    annotatedTag += token.name;
+    annotatedTag += ">";
 
-    return annotatedText;
+    return annotatedTag;
 }
 
-function handleCloseTagToken(annotatedText: string, token: CloseTagToken, tagStack: (OpenTagToken | CloseTagToken)[]): string {
+function handleCloseTagToken(token: CloseTagToken, tagStack: (OpenTagToken | CloseTagToken)[]): string {
     let openToken = tagStack.pop();
 
     if (openToken === undefined || openToken.name !== token.name) {
         throw new AreaParenthesizationError(AreaParenthesizationError.IMPROPER_PARENTHESIZATION, token.name);
     }
+    let annotatedTag: string = "";
+    
+    annotatedTag += "</";
+    annotatedTag += token.name;
+    annotatedTag += ">";
 
-    annotatedText += "</";
-    annotatedText += token.name;
-    annotatedText += ">";
+    return annotatedTag;
+}
+
+function getLines(text: string): string[] {
+    return text.split('\n');
+}
+
+function getLeadingWhiteChars(line: string): string {
+    let currentChar = line[0];
+    let index: number = 1;
+    let whole: string = "";
+    while ((currentChar === ' ' || currentChar === '\t') && index !== line.length) {
+        whole += currentChar;
+        currentChar = line[index];
+        index++;
+    }
+    return whole;
+}
+
+function getHeadingLevel(line: string): number | null {
+    let currentChar = line[0];
+    let index = 1;
+    while (currentChar == '#') {
+        currentChar = line[index];
+        index++;
+    }
+
+    return index <= 6 ? index : null;
+}
+
+function processLine(line: string): string {
+    let annotatedLine: string = "";
+
+    let whiteChars = getLeadingWhiteChars(line);
+    let whiteSpaceLen = whiteChars.length;
+
+    console.log("WhiteSpaceLen: " + whiteSpaceLen);
+    // # counts as a heading only at the beginning of the line (excluding white characters)
+    let headingLevel: number | null = null;
+    if (line[whiteSpaceLen] === '#') {
+        headingLevel = getHeadingLevel(line.substring(annotateText.length));
+    }
+    console.log("HeadingLevel: " + headingLevel);
+    
+    annotatedLine += (headingLevel !== null) ? "<heading" + headingLevel + ">" : whiteChars;
+
+    // TODO: variable number of white spaces after #
+
+    let normalTextStart = (headingLevel !== null) ? whiteSpaceLen + headingLevel + 1 : whiteSpaceLen;
+    for (let i = normalTextStart; i < line.length; i++) {
+        let char = line[i];
+
+        // TODO
+
+        annotatedLine += char;
+    }
+
+    if (headingLevel !== null) {
+        annotatedLine += "</heading" + headingLevel + ">";
+    }
+
+    return annotatedLine;
+}
+
+function handleMarkdownText(text: string): string {
+    let lines: string[] = getLines(text);
+    let annotatedText: string = "";
+    
+    lines.forEach((line) => {
+        annotatedText += processLine(line);
+        annotatedText += '\n';
+    });
 
     return annotatedText;
 }
@@ -63,15 +138,14 @@ export function annotateText(tokenArray: Token[]): string {
     // Transform markdown tags into annotation
 
     tokenArray.forEach((token) => {
-        console.log(token);
         if (isOpenTagToken(token)) {
-            annotatedText = handleOpenTagToken(annotatedText, token, tagStack);
+            annotatedText += handleOpenTagToken(token, tagStack);
             return; //continue
         } else if (isCloseTagToken(token)) {
-            annotatedText = handleCloseTagToken(annotatedText, token, tagStack);
+            annotatedText += handleCloseTagToken(token, tagStack);
             return; //continue
         }
-        annotatedText += token;
+        annotatedText += handleMarkdownText(token);
     });
 
     if (tagStack.length !== 0) {
