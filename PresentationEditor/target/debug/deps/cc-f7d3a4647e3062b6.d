08a10c26e71af0eb6df15e728b6ff3d74463de07
Jan Čelovský <celovskyhonza@gmail.com>
2024-01-20 17:27:48 +0100

    Move editor to its own folder

---
+++ PresentationEditor/target/debug/deps/cc-f7d3a4647e3062b6.d
@@ -0,0 +1,10 @@
+/home/olcor/OpenPresentationsForEducation/target/debug/deps/libcc-f7d3a4647e3062b6.rmeta: /home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/lib.rs /home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/os_pipe.rs /home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/os_pipe/unix.rs /home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/windows_registry.rs
+
+/home/olcor/OpenPresentationsForEducation/target/debug/deps/libcc-f7d3a4647e3062b6.rlib: /home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/lib.rs /home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/os_pipe.rs /home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/os_pipe/unix.rs /home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/windows_registry.rs
+
+/home/olcor/OpenPresentationsForEducation/target/debug/deps/cc-f7d3a4647e3062b6.d: /home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/lib.rs /home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/os_pipe.rs /home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/os_pipe/unix.rs /home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/windows_registry.rs
+
+/home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/lib.rs:
+/home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/os_pipe.rs:
+/home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/os_pipe/unix.rs:
+/home/olcor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cc-1.0.83/src/windows_registry.rs:
---
+++ PresentationEditor/target/debug/deps/tree_sitter_markdown_slides-78254387a03bef2e.d
@@ -0,0 +1,6 @@
+/home/olcor/OpenPresentationsForEducation/target/debug/deps/libtree_sitter_markdown_slides-78254387a03bef2e.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/olcor/OpenPresentationsForEducation/target/debug/deps/tree_sitter_markdown_slides-78254387a03bef2e.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+bindings/rust/lib.rs:
+bindings/rust/../../src/node-types.json:
