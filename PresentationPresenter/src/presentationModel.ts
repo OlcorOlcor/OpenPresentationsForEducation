18ac9e07a746e61e7dc50d36fcf22fdb2b3d2254
Jan Čelovský <celovskyhonza@gmail.com>
2024-02-14 16:06:29 +0100

    Remake json structure into OOP

--- PresentationPresenter/src/presentationModel.ts
+++ PresentationPresenter/src/presentationModel.ts
@@ -1,75 +1,150 @@
-import { NullLiteral } from "typescript";
+interface IVisitor {
+    visitTextNode(element: TextElement): string;
+    visitBoldNode(element: BoldElement): string;
+    visitItalicNode(element: ItalicElement): string;
+    visitBoldItalicNode(element: BoldItalicElement): string;
+    visitCodeNode(element: CodeElement): string;
+    visitImageNode(element: ImageElement): string;
+    visitLinkNode(element: LinkElement): string;
+    visitParagraphNode(element: ParagraphElement): string;
+    visitHeadingNode(element: HeadingElement): string;
+    visitListNode(element: ListElement): string;
+    visitListItemNode(element: ListItemElement): string;
+    visitBlockQuoteNode(element: BlockQuoteElement): string;
+    visitSlideNode(element: SlideElement): string;
+}
 
-export type Element = TextAnnotation | Paragraph | HeadingElement | ListItem;
+interface IVisitable {
+    accept(visitor: IVisitor): void;
+}
 
-export type OuterElement = Paragraph | HeadingElement | List | BlockQuote;
+class Element { }
 
-export type InlineElement = TextAnnotation | Link | Image;
-
-export type Text = {
-    type: string;
-    content: [string];
-    attributes?: {};
-};
-
-export type TextAnnotation = {
-    type: string;
-    content: [Text | TextAnnotation];
-    attributes?: {};
-};
-
-export type Link = {
-    type: string;
-    content: [string];
-    attributes: {
-        alias: string;
-    };
-};
-
-export type Image = {
-    type: string;
-    content: [string];
-    attributes: {
-        alias: string;
-    };
-};
-
-export type ListItem = {
-    type: string;
-    content: [Text | TextAnnotation];
-    attributes?: {};
-};
-
-export type List = {
-    type: string;
-    content: [List | ListItem];
-    attributes: {
-        listType: string;
-    };
-};
-
-export type Paragraph = {
-    type: string;
+class ListItemElement extends Element {
     content: [Text | InlineElement];
-    attributes?: {};
-};
+    public constructor(content: [Text | InlineElement]) {
+        super();
+        this.content = content;
+    }
+}
 
-export type HeadingElement = {
-    type: string;
+class TextElement extends Element {
+    content: string;
+    public constructor(content: string) {
+        super();
+        this.content = content;
+    }
+}
+
+class InlineElement extends Element {
     content: [Text | InlineElement];
-    attributes: {
-        level: number;
-    };
-};
+    public constructor(content: [Text | InlineElement]) {
+        super();
+        this.content = content;
+    }
+ }
+class BoldElement extends InlineElement implements IVisitable {
+    accept(visitor: IVisitor): void {
+        visitor.visitBoldNode(this);
+    }
+    
+}
 
-export type BlockQuote = {
-    type: string;
-    content: [OuterElement];
-    attributes?: {};
-};
+class ItalicElement extends InlineElement implements IVisitable {
+    accept(visitor: IVisitor): void {
+        visitor.visitItalicNode(this);
+    }
 
-export type Slide = {
-    type: string;
+}
+
+class BoldItalicElement extends InlineElement implements IVisitable {
+    accept(visitor: IVisitor): void {
+        visitor.visitBoldItalicNode(this);
+    }
+}
+
+class CodeElement extends InlineElement implements IVisitable {
+    accept(visitor: IVisitor): void {
+        visitor.visitCodeNode(this);
+    }
+}
+
+class LinkElement extends InlineElement implements IVisitable {
+    accept(visitor: IVisitor): void {
+        visitor.visitLinkNode(this);
+    }
+}
+
+class ImageElement extends InlineElement implements IVisitable {
+    accept(visitor: IVisitor): void {
+        visitor.visitImageNode(this);
+    }
+}
+
+class OuterElement extends Element { }
+class ParagraphElement extends OuterElement implements IVisitable {
+    content: [TextElement | InlineElement];
+    public constructor(content: [TextElement | InlineElement]) {
+        super();
+        this.content = content;
+    }
+    accept(visitor: IVisitor): void {
+        visitor.visitParagraphNode(this);
+    }
+}
+
+class HeadingElement extends OuterElement implements IVisitable {
+    content: [TextElement | InlineElement];
+    level: number;
+
+    public constructor(level: number, content: [TextElement | InlineElement]) {
+        super();
+        this.content = content;
+        this.level = level;
+    }
+
+    accept(visitor: IVisitor): void {
+        visitor.visitHeadingNode(this);
+    }
+}
+
+class ListElement extends OuterElement implements IVisitable {
+    content: [ListItemElement | ListElement];
+    listType: string;
+
+    public constructor(listType: string, content: [ListItemElement | ListElement]) {
+        super();
+        this.content = content;
+        this.listType = listType;
+    }
+
+    accept(visitor: IVisitor): void {
+        visitor.visitListNode(this);
+    }
+}
+
+class BlockQuoteElement extends OuterElement implements IVisitable {
     content: [OuterElement];
-    attributes?: {};
-};
+
+    public constructor(content: [OuterElement]) {
+        super();
+        this.content = content;
+    }
+
+    accept(visitor: IVisitor): void {
+        visitor.visitBlockQuoteNode(this);
+    }
+}
+
+class SlideElement extends Element implements IVisitable {
+    content: [OuterElement];
+    
+    public constructor(content: [OuterElement]) {
+        super();
+        this.content = content;
+    }
+
+    accept(visitor: IVisitor): void {
+        visitor.visitSlideNode(this);
+    }
+}
