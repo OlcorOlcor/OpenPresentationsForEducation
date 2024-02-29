f78cf97fddfdb47e83c067eaa9b397db88514f1e
Jan Čelovský <celovskyhonza@gmail.com>
2024-02-29 12:37:06 +0100

    update type structure

--- PresentationPresenter/src/presentationTypes.ts
+++ PresentationPresenter/src/presentationTypes.ts
@@ -6,54 +6,54 @@ export type InlineElement = TextAnnotation | Link | Image;
 
 export type Text = {
     type: string;
-    content: [string];
+    content: string[];
 };
 
 export type TextAnnotation = {
     type: string;
-    content: [Text | TextAnnotation];
+    content: (Text | TextAnnotation)[];
 };
 
 export type Link = {
     type: string;
-    content: [string];
+    content: string[];
     attributes: { alias: string };
 };
 
 export type Image = {
     type: string;
-    content: [string];
+    content: string[];
     attributes: { alias: string };
 };
 
 export type ListItem = {
     type: string;
-    content: [Text | TextAnnotation];
+    content: (Text | TextAnnotation)[];
 };
 
 export type List = {
     type: string;
-    content: [List | ListItem];
+    content: (List | ListItem)[];
     attributes: { listType: string };
 };
 
 export type Paragraph = {
     type: string;
-    content: [Text | InlineElement];
+    content: (Text | InlineElement)[];
 };
 
 export type HeadingElement = {
     type: string;
-    content: [Text | InlineElement];
+    content: (Text | InlineElement)[];
     attributes: {level: number}
 };
 
 export type BlockQuote = {
     type: string;
-    content: [OuterElement];
+    content: OuterElement[];
 };
 
 export type Slide = {
     type: string;
-    content: [OuterElement];
+    content: OuterElement[];
 };
