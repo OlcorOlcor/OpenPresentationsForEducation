a516b3c6f73c10e1cea97990ee5370a180481f94
Jan Čelovský <celovskyhonza@gmail.com>
2024-03-14 17:45:56 +0100

    render html

--- PresentationEditor/src/Preview.tsx
+++ PresentationEditor/src/Preview.tsx
@@ -8,23 +8,37 @@ interface PreviewProps {
 const Preview: React.FC<PreviewProps> = ({ fetchHtml, fetchJson } ) => {
   const [data, setData] = useState<string>("");
 
+  const [code, setCode] = useState<boolean>(false);
   function getHtml() {
+    setCode(false);
+    setData(fetchHtml());
+  }
+
+  function getHtmlCode() {
+    setCode(true);
     setData(fetchHtml());
   }
 
   function getJson() {
+    setCode(false);
     setData(fetchJson());
   }
 
   return (
     <div>
       <nav>
-        <button onClick={() => getHtml()}>html</button>
+        <button onClick={() => getHtmlCode()}>html</button>
+        <button onClick={() => getHtml()}>html - code</button>
         <button onClick={() => getJson()}>json</button>
       </nav>
-      <div>
-          {data}
-      </div>
+      {!code && 
+        <div>
+            {data}
+        </div>
+      }
+      {code && 
+        <div dangerouslySetInnerHTML={{__html: data}}></div>
+      }
     </div>
   );
 }
