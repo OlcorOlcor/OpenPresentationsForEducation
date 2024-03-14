24cc39dd44ca693459921613490111685c4ea8a6
Jan Čelovský <celovskyhonza@gmail.com>
2024-03-14 13:31:11 +0100

    Add functions to MarkdownParser

--- PresentationPresenter/src/markdownParser.ts
+++ PresentationPresenter/src/markdownParser.ts
@@ -1,8 +1,11 @@
-import { getPositionOfLineAndCharacter } from "typescript";
 import * as pt from "./presentationTypes"; 
 import markdownit, { Token } from "markdown-it";
 
+class RefIndex {
+    public index: number = 0;
+}
 
+// TODO: Whole class needs refactoring
 export class MarkdownParser {
 
     public parseMarkdown(markdown: string): pt.Slide[] {
@@ -17,22 +20,22 @@ export class MarkdownParser {
 
     private handleArray(array: Token[]): pt.Slide {
         let slide: pt.Slide = { type: "slide", content: []};
-        for (let i = 0; i < array.length; ++i) {
-            switch (array[i].type) {
+        for (let index: RefIndex = new RefIndex(); index.index < array.length; ++index.index) {
+            switch (array[index.index].type) {
                 case "bullet_list_open":
-                    // TODO
+                    slide.content.push(this.handleList(array, index, false));
                 break;
                 case "ordered_list_open":
-                    // TODO
+                    slide.content.push(this.handleList(array, index, true));
                 break;
                 case "paragraph_open":
-                    slide.content.push(this.handleParagraph(array, i));
+                    slide.content.push(this.handleParagraph(array, index));
                 break;
                 case "heading_open":
-                    // TODO
+                    slide.content.push(this.handleHeading(array, index, array[index.index].markup.length));
                 break;
                 case "blockquote_open":
-                    // TODO
+                    slide.content.push(this.handleBlockQuote(array, index));
                 break;
                 default:
                 break;
@@ -41,17 +44,91 @@ export class MarkdownParser {
         return slide;
     }
 
-    private handleParagraph(array: Token[], index: number): pt.Paragraph {
-        let paragraph: pt.Paragraph = {type: "paragraph", content: []};
-        for (index + 1; index < array.length; ++index) {
-            if (array[index].type == "paragraph_close") {
+    private handleBlockQuote(array: Token[], index: RefIndex): pt.BlockQuote {
+        let blockQuote: pt.BlockQuote = { type: "blockquote", content: [] };
+        let done: boolean = false;
+        for (; index.index < array.length; ++index.index) {
+            switch (array[index.index].type) {
+                case "bullet_list_open":
+                    blockQuote.content.push(this.handleList(array, index, false));
+                break;
+                case "ordered_list_open":
+                    blockQuote.content.push(this.handleList(array, index, true));
+                break;
+                case "paragraph_open":
+                    blockQuote.content.push(this.handleParagraph(array, index));
+                break;
+                case "heading_open":
+                    blockQuote.content.push(this.handleHeading(array, index, array[index.index].markup.length));
+                break;
+                case "blockquote_open":
+                    blockQuote.content.push(this.handleBlockQuote(array, index));
+                break;
+                case "blockquote_close":
+                    done = true;
                 break;
             }
-            if (array[index].type == "inline") {
-                this.getInline(array[index]).forEach(item => paragraph.content.push(item));
+            if (done) {
+                break;
+            }
+        }
+        return blockQuote;
+    }
+    
+    private handleParagraph(array: Token[], index: RefIndex): pt.Paragraph {
+        let paragraph: pt.Paragraph = {type: "paragraph", content: []};
+        for (index.index + 1; index.index < array.length; ++index.index) {
+            if (array[index.index].type === "paragraph_close") {
+                break;
+            }
+            if (array[index.index].type === "inline") {
+                this.getInline(array[index.index]).forEach(item => paragraph.content.push(item));
             }
         }
         return paragraph;
+    }
+
+    private handleHeading(array: Token[], index: RefIndex, level: number): pt.HeadingElement {
+        let heading: pt.HeadingElement = {type: "heading", content: [], attributes: {level: level}}
+        for (index.index + 1; index.index < array.length; ++index.index) {
+            if (array[index.index].type === "heading_close") {
+                break;
+            }
+            if (array[index.index].type === "inline") {
+                this.getInline(array[index.index]).forEach(item => heading.content.push(item));
+            }
+        }
+        return heading;
+    }
+
+    private handleList(array: Token[], index: RefIndex, ordered: boolean): pt.List {
+        let list: pt.List = { type: "list", content: [], attributes: { listType: (ordered) ? "ordered" : "unordered"}};
+        let done: boolean = false;
+        for (index.index + 1; index.index < array.length; ++index.index) {
+            switch (array[index.index].type) {
+                case "bullet_list_close":
+                case "ordered_list_close":
+                    done = true;
+                break;
+                case "bullet_list_open":
+                    list.content.push(this.handleList(array, index, false));
+                break;
+                case "ordered_list_open":
+                    list.content.push(this.handleList(array, index, true));
+                break;
+                case "list_item_open":
+                    index.index++; // paragraph_open
+                    index.index++; // inline
+                    let listItem: pt.ListItem = { type: "listItem", content: []};
+                    this.getInline(array[index.index]).forEach(item => listItem.content.push(item));
+                    list.content.push(listItem);
+                break;
+            }
+            if (done) {
+                break;
+            }
+        }
+        return list;
     }
 
     private getInline(inline: Token): (pt.InlineElement | pt.Text)[] {
@@ -116,9 +193,4 @@ export class MarkdownParser {
         return inlineElements;
     }
 
-    private handleList(array: Token[], index: number, ordered: boolean): pt.List {
-        let list: pt.List = { type: "list", content: [], attributes: { listType: (ordered) ? "ordered" : "unordered"}};
-        // TODO
-        return list;
-    }
 }
