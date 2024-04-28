914bc73ecffbd32bf27a4e457e788ec789ff142f
Jan Čelovský <celovskyhonza@gmail.com>
2024-04-28 19:13:25 +0200

    Add import and export buttons

--- PresentationEditor/src/Editor/Menu.tsx
+++ PresentationEditor/src/Editor/Menu.tsx
@@ -6,9 +6,10 @@ interface MenuProps {
     addLane(): void;
     swapLane(): void;
     importPresentation(file: File): void;
+    exportPresentation(): void;
 }
 
-const Menu: React.FC<MenuProps> = ({addLane, swapLane, importPresentation}) => {
+const Menu: React.FC<MenuProps> = ({addLane, swapLane, importPresentation, exportPresentation}) => {
     return (
         <div className="menu">
             <div>
@@ -18,7 +19,11 @@ const Menu: React.FC<MenuProps> = ({addLane, swapLane, importPresentation}) => {
                 <Button variant="contained" onClick={swapLane}>Swap Lanes</Button>
             </div>
             <div>
+                <label htmlFor="fileInput">Import Lanes: </label>
                 <input type="file" id="fileInput" onChange={(e) => importPresentation(e.target.files?.[0]!)} />
+            </div>
+            <div>
+                <Button variant="contained" onClick={exportPresentation}>Export</Button>
             </div>
         </div>
     )
